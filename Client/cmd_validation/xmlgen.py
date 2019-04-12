247f397d4caaeea06d7c6e1cc62e325a25119371
Jyuqi <48507550+Jyuqi@users.noreply.github.com>
2019-04-12 15:46:01 +0800

    add xmlgen.py
    convert mhw_vdbox_vdenc_hwcmd_g12_tglhp.h into convert mhw_vdbox_vdenc_hwcmd_g12_tglhp.h.xml

---
+++ Client/cmd_validation/xmlgen.py
@@ -0,0 +1,28 @@
+#import xml.etree.ElementTree as ET
+from ult_generator import header_parser
+import re
+import os
+
+
+def main(input_file='input.txt', media_path='C:\\Users\\jiny\\gfx\\gfx-driver\\Source\\media\\\media_driver\\agnostic'):
+       with open(input_file, 'r') as fin:
+        for line in fin:
+            line = line.strip()
+            if not line:
+                continue
+            idx = line.rfind('/')
+            file_name = line[idx+1:]
+            file_path = line[:idx+1]
+            parser_list = [header_parser.HeaderParser(file_name, file_path)]
+            # print(os.getcwd())
+            parser_list[0].read_file()
+            xml_tree_str = parser_list[0].parse_file_info()
+            with open( file_name + '.xml', "w") as f:
+                f.write(xml_tree_str)
+
+
+
+
+
+if __name__ == '__main__':
+    main()
